ff9a6a4c5d4125b54203a8116307cbb032dc1f47
Hansol YOO <78459995+yhansol145@users.noreply.github.com>
2021-04-18 01:22:06 +0900

    Add files via upload

---
+++ project1/PhoneBookVer10.java
@@ -0,0 +1,100 @@
+package project1;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import project1.ver10.PhoneBookManager;
+import project1.ver10.PuzzleGame;
+import project1.ver10.AutoSaverT;
+import project1.ver10.MenuItem;
+import project1.ver10.MenuSelectException;
+
+public class PhoneBookVer10  {
+	
+	public static void main(String[] args) {
+		
+		PuzzleGame game = new PuzzleGame();
+		PhoneBookManager manager = new PhoneBookManager();
+		ArrayList<AutoSaverT> save = new ArrayList<AutoSaverT>();
+		int index=0;
+		int choice;
+		Scanner scan = new Scanner(System.in);
+		manager.callPhoneBook();
+		
+		while(true) {
+			try {
+				manager.printMenu();
+				choice = scan.nextInt();
+				
+				if(choice>7 || choice<1) {
+					MenuSelectException ex = new MenuSelectException();
+					throw ex;
+				}
+				
+				switch(choice) {
+				case MenuItem.ONE:
+					manager.dataInput(choice);
+					System.out.println();
+					break;
+					
+				case MenuItem.TWO:
+					manager.dataSearch();
+					System.out.println();
+					continue;
+					
+				case MenuItem.THR:
+					manager.dataDelete();
+					System.out.println();
+					continue;
+					
+				case MenuItem.FOU:
+					manager.dataAllShow();
+					System.out.println();
+					continue;
+					
+				case MenuItem.FIV:
+					System.out.println("== 저장옵션 선택 ==");
+					System.out.println("저장옵션을 선택하세요.");
+					System.out.println("1. 자동저장On,   2. 자동저장Off");
+					choice = scan.nextInt();
+					if(choice==1) {
+						save.add(new AutoSaverT(manager));
+						save.get(index).setDaemon(true);
+						save.get(index).start();
+						System.out.println("자동저장을 시작합니다.");
+					}
+					else if(choice==2) {
+						save.get(index++).interrupt();
+						System.out.println("자동저장이 종료됩니다.");
+					}
+					break;
+				case MenuItem.SIX:
+					game.mainPrint();
+					
+				case MenuItem.SEV:
+					manager.savePhoneBook();
+					System.out.println("프로그램을 종료합니다.");
+					System.exit(choice);
+				
+				}
+			}
+			catch(MenuSelectException e) {
+				System.out.println(e.getMessage());
+			}
+			catch(InputMismatchException e) {
+				e.printStackTrace();
+				scan.nextLine();
+			}
+			catch(NullPointerException e) {
+				e.printStackTrace();
+				scan.nextLine();
+			}
+			catch(IllegalThreadStateException e) {
+				System.out.println("이미 자동저장이 실행중입니다.");
+			}
+			catch(IndexOutOfBoundsException e) {
+				System.out.println("자동저장이 실행되고 있지 않습니다.");
+			}
+		}
+	}
+}
